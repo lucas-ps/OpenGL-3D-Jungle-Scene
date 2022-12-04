c8fc68e7d6cc47516c3b9f3fbbcb1578953ad400
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2022-12-04 01:31:38 +0000

    Implemented shadows

---
+++ main/renderer.py
@@ -0,0 +1,34 @@
+class Renderer:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.link = app.link
+        self.scene = app.scene
+
+        # Depth buffer for shadows
+        self.depth_texture = self.link.texture.textures['depth_texture']
+        self.depth_framebuffer = self.ctx.framebuffer(depth_attachment=self.depth_texture)
+
+    def render_shadow(self):
+        """
+        Renders the scene's shadows
+        """
+        self.depth_framebuffer.clear()
+        self.depth_framebuffer.use()
+        for obj in self.scene.objects:
+            obj.render_shadow()
+
+    def render(self):
+        """
+        Renders each object in the object list, then the skybox.
+        """
+        self.app.ctx.screen.use()
+        self.render_shadow()
+        self.scene.update()
+        self.app.ctx.screen.use()
+        for obj in self.scene.objects:
+            obj.render()
+        self.scene.skybox.render()
+
+    def destroy(self):
+        self.depth_framebuffer.release()
